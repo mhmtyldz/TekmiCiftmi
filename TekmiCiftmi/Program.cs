eefff98f2b561aaaf05e9960352ee1b5d2cacf7a
Mahmut Yıldız <yldzmahmut0@gmail.com>
2017-08-03 16:40:03 +0300

    1. Değiiklik Yapıldı
    2 Yöntem kullanıldı 2. Pek uygun olmadı soruya onu o yüzden yorum
    satırına aldım

---
+++ TekmiCiftmi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekmiCiftmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            string[] TekCift = { "ÇİFT", "TEK" };
+
+            Console.Write("Sayı giriniz!");
+            sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine(sayi + " Sayısı " + TekCift[sayi % 2] + " Bir Sayıdır!");
+
+
+
+
+
+
+
+          //  Switchte sonucta bi if yapısı sayılır ama gene de 2. Yöntem olarak yazmak istedim.
+            // ***************************************************************************************************************
+            //Console.Write("Sayı giriniz!");
+            //sayi = int.Parse(Console.ReadLine());
+
+
+            //switch (sayi%2)
+            //{
+            //    case 0:
+            //        Console.WriteLine(sayi + " Çift Bir Sayıdır!");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine(sayi + " Tek Bir Sayıdır!");
+            //        break;
+            //}
+
+            // ***************************************************************************************************************
+
+        }
+    }
+}
